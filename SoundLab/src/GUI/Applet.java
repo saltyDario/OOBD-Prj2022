e8829aeaa2d80110960d2b819a9c27d7bd6a4a6c
saltyDario <94072049+saltyDario@users.noreply.github.com>
2021-12-12 00:04:14 +0100

    V0.2.4 registrazione e login sistemi in altre classi

---
+++ SoundLab/src/GUI/Applet.java
@@ -0,0 +1,350 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.ActionEvent;
+import javax.swing.JMenuBar;
+import javax.swing.JOptionPane;
+
+import Modelli.Utente;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.SwingUtilities;
+
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Color;
+import java.awt.SystemColor;
+import javax.swing.UIManager;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.CompoundBorder;
+import javax.swing.event.MouseInputAdapter;
+import javax.swing.border.LineBorder;
+import GUI.FrontGUI;
+import java.awt.event.MouseMotionAdapter;
+
+public class Applet {
+
+	private PanelHome paneHome;
+	private PanelLibrary paneLibrary;
+	private PanelSearch paneSearch;
+	private PanelInfo paneInfo;
+	private int mouseX, mouseY;
+	
+	public Applet(String uname, String psd) {
+		initialize(uname, psd);
+	}
+
+
+	private void initialize(String uname, String psd) {
+		Utente utente = new Utente(uname , psd);
+		JFrame AppWindow = new JFrame();
+		AppWindow.setResizable(false);
+		AppWindow.setUndecorated(true);
+		AppWindow.getContentPane().setBackground(Color.GRAY);
+		AppWindow.setTitle("SoundLab");
+		AppWindow.setVisible(true);
+		AppWindow.setBounds(100, 100, 823, 616);
+		AppWindow.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		AppWindow.getContentPane().setLayout(null);
+		
+		paneHome = new PanelHome();
+		paneHome.setBorder(new LineBorder(Color.BLACK, 2, true));
+		paneHome.setBounds(0, 0, 486, 588);
+		paneLibrary = new PanelLibrary();
+		paneLibrary.setBorder(new LineBorder(Color.BLACK, 2, true));
+		paneLibrary.setBounds(0, 0, 486, 588);
+		paneSearch = new PanelSearch();
+		paneSearch.setBorder(new LineBorder(Color.BLACK, 2, true));
+		paneSearch.setBounds(0, 0, 486, 588);
+		paneInfo = new PanelInfo();
+		paneInfo.setBorder(new LineBorder(Color.BLACK, 2, true));
+		paneInfo.setBounds(0, 0, 486, 588);
+		
+		JPanel draggablePanel = new JPanel();
+		draggablePanel.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				AppWindow.setLocation(AppWindow.getX() + e.getX() - mouseX, AppWindow.getY() + e.getY() - mouseY);
+			}
+		});
+		draggablePanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		draggablePanel.setLayout(null);
+		draggablePanel.setBackground(Color.BLACK);
+		draggablePanel.setBounds(-2, 0, 825, 20);
+		AppWindow.getContentPane().add(draggablePanel);
+		
+		JLabel exitButton = new JLabel("");
+		exitButton.setBounds(798, -2, 27, 24);
+		draggablePanel.add(exitButton);
+		exitButton.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/closered.png")).getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH)));
+		exitButton.setForeground(Color.WHITE);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "Sei sicuro di voler uscire?", "Conferma", JOptionPane.YES_NO_OPTION) == 0) {
+					System.exit(0);
+				}
+			}
+		});
+		
+		exitButton.setHorizontalAlignment(SwingConstants.CENTER);
+		exitButton.setFont(new Font("Arial", Font.BOLD, 16));
+		
+		JLabel minimizeButton = new JLabel("");
+		minimizeButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				AppWindow.setExtendedState(JFrame.ICONIFIED);
+			}
+		});
+		minimizeButton.setBounds(769, -3, 30, 27);
+		draggablePanel.add(minimizeButton);
+		minimizeButton.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/minimize.png")).getImage().getScaledInstance(18, 18, Image.SCALE_SMOOTH)));
+		minimizeButton.setHorizontalAlignment(SwingConstants.CENTER);
+		minimizeButton.setForeground(Color.WHITE);
+		minimizeButton.setFont(new Font("Arial", Font.BOLD, 16));
+		
+		JPanel panel_Menu = new JPanel();
+		panel_Menu.setBackground(Color.BLACK);
+		panel_Menu.setBounds(0, 0, 327, 616);
+		AppWindow.getContentPane().add(panel_Menu);
+		panel_Menu.setLayout(null);
+		
+		JLabel lbllconLogo = new JLabel("");
+		lbllconLogo.setHorizontalAlignment(SwingConstants.CENTER);
+		lbllconLogo.setForeground(new Color(0, 255, 255));
+		lbllconLogo.setBounds(0, 20, 327, 205);
+		panel_Menu.add(lbllconLogo);
+		lbllconLogo.setIcon(new ImageIcon(FrontGUI.class.getResource("/Immagini/FrontLogo.png")));
+		
+		JPanel panel_Home = new JPanel();
+		panel_Home.addMouseListener(new PanelButtonMouseAdapter(panel_Home) {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				menuClicked(paneHome);
+			}
+		});
+		panel_Home.setBounds(0, 251, 327, 65);
+		panel_Menu.add(panel_Home);
+		panel_Home.setBorder(new LineBorder(Color.BLACK, 2, true));
+		panel_Home.setBackground(Color.GRAY);
+		panel_Home.setLayout(null);
+		
+		JLabel homeLabel = new JLabel("HOME");
+		homeLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		homeLabel.setForeground(new Color(245, 245, 245));
+		homeLabel.setFont(new Font("Arial", Font.BOLD, 27));
+		homeLabel.setBounds(0, 0, 327, 65);
+		panel_Home.add(homeLabel);
+		
+		JLabel homeIcon = new JLabel("");
+		homeIcon.setBounds(56, 11, 50, 40);
+		panel_Home.add(homeIcon);
+		homeIcon.setHorizontalAlignment(SwingConstants.CENTER);
+		homeIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/musichome.png")).getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH)));
+		
+		JPanel panel_Library = new JPanel();
+		panel_Library.addMouseListener(new PanelButtonMouseAdapter(panel_Library){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				menuClicked(paneLibrary);
+			}
+		});
+		panel_Library.setBounds(0, 314, 327, 65);
+		panel_Menu.add(panel_Library);
+		panel_Library.setBackground(Color.GRAY);
+		panel_Library.setBorder(new LineBorder(Color.BLACK, 2, true));
+		panel_Library.setLayout(null);
+		
+		JLabel libraryLabel = new JLabel("LIBRARY");
+		libraryLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		libraryLabel.setForeground(new Color(245, 245, 245));
+		libraryLabel.setFont(new Font("Arial", Font.BOLD, 27));
+		libraryLabel.setBounds(0, 0, 327, 65);
+		panel_Library.add(libraryLabel);
+		
+		JLabel libraryIcon = new JLabel("");
+		libraryIcon.setBounds(56, 11, 50, 40);
+		panel_Library.add(libraryIcon);
+		libraryIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/library.png")).getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH)));
+		libraryIcon.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JPanel panel_Search = new JPanel();
+		panel_Search.addMouseListener(new PanelButtonMouseAdapter(panel_Search){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				menuClicked(paneSearch);
+			}
+		});
+		panel_Search.setBounds(0, 377, 327, 65);
+		panel_Menu.add(panel_Search);
+		panel_Search.setBackground(Color.GRAY);
+		panel_Search.setBorder(new BevelBorder(BevelBorder.RAISED, Color.BLACK, Color.BLACK, Color.BLACK, Color.BLACK));
+		panel_Search.setLayout(null);
+		
+		JLabel searchLabel = new JLabel("SEARCH");
+		searchLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		searchLabel.setForeground(new Color(245, 245, 245));
+		searchLabel.setFont(new Font("Arial", Font.BOLD, 27));
+		searchLabel.setBounds(0, 0, 327, 65);
+		panel_Search.add(searchLabel);
+		
+		JLabel searchIcon = new JLabel("");
+		searchIcon.setBounds(56, 11, 50, 40);
+		panel_Search.add(searchIcon);
+		searchIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/searchb.png")).getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH)));
+		searchIcon.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JPanel panel_SignOut = new JPanel();
+		panel_SignOut.addMouseListener(new PanelButtonMouseAdapter(panel_SignOut) {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				
+				if(JOptionPane.showConfirmDialog(null, "Sei sicuro di voler uscire?", "Conferma", JOptionPane.YES_NO_OPTION) == 0) {
+					FrontGUI FrontGUI = new FrontGUI();
+					AppWindow.setVisible(false);
+					AppWindow.dispose();	
+				}
+			}
+		});
+		panel_SignOut.setBounds(0, 440, 327, 65);
+		panel_Menu.add(panel_SignOut);
+		panel_SignOut.setBackground(Color.GRAY);
+		panel_SignOut.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel_SignOut.setLayout(null);
+		
+		JLabel signOutLabel = new JLabel("SIGN OUT");
+		signOutLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		signOutLabel.setForeground(new Color(245, 245, 245));
+		signOutLabel.setFont(new Font("Arial", Font.BOLD, 27));
+		signOutLabel.setBounds(0, 0, 327, 65);
+		panel_SignOut.add(signOutLabel);
+		
+		JLabel signOutIcon = new JLabel("");
+		signOutIcon.setBounds(57, 11, 50, 40);
+		panel_SignOut.add(signOutIcon);
+		signOutIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/logout.png")).getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH)));
+		signOutIcon.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JPanel panel_Info = new JPanel();
+		panel_Info.addMouseListener(new PanelButtonMouseAdapter(panel_Info){
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				menuClicked(paneInfo);
+			}
+		});
+		panel_Info.setBounds(0, 503, 327, 65);
+		panel_Menu.add(panel_Info);
+		panel_Info.setLayout(null);
+		panel_Info.setBorder(new LineBorder(new Color(0, 0, 0), 2));
+		panel_Info.setBackground(Color.GRAY);
+		
+		JLabel infoLabel = new JLabel("INFO");
+		infoLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		infoLabel.setForeground(new Color(245, 245, 245));
+		infoLabel.setFont(new Font("Arial", Font.BOLD, 27));
+		infoLabel.setBounds(0, 0, 327, 65);
+		panel_Info.add(infoLabel);
+		
+		JLabel infoIcon = new JLabel("");
+		infoIcon.setBounds(56, 11, 50, 40);
+		panel_Info.add(infoIcon);
+		infoIcon.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/group.png")).getImage().getScaledInstance(40, 40, Image.SCALE_SMOOTH)));
+		infoIcon.setHorizontalAlignment(SwingConstants.CENTER);
+		
+		JPanel paneMainContent = new JPanel();
+		paneMainContent.setBounds(332, 24, 486, 588);
+		AppWindow.getContentPane().add(paneMainContent);
+		paneMainContent.setLayout(null);
+		
+		paneMainContent.add(paneHome);
+		paneMainContent.add(paneHome);
+		paneMainContent.add(paneLibrary);
+		paneMainContent.add(paneSearch);
+		paneMainContent.add(paneInfo);
+		
+		menuClicked(paneHome);
+		
+		JPanel panel_welcome = new JPanel();
+		panel_welcome.setBorder(new LineBorder(Color.BLACK, 2));
+		panel_welcome.setBackground(Color.GRAY);
+		panel_welcome.setBounds(0, 225, 327, 28);
+		panel_Menu.add(panel_welcome);
+		panel_welcome.setLayout(null);
+		
+		
+		JLabel welcomeLabel = new JLabel("Benvenuto:");
+		welcomeLabel.setBounds(10, -19, 101, 68);
+		panel_welcome.add(welcomeLabel);
+		welcomeLabel.setForeground(new Color(255, 255, 255));
+		welcomeLabel.setBackground(Color.WHITE);
+		welcomeLabel.setFont(new Font("Arial", Font.BOLD, 14));
+		
+		JLabel userLabel = new JLabel(utente.getUsername());
+		userLabel.setForeground(new Color(255, 255, 255));
+		userLabel.setBounds(98, -4, 106, 39);
+		panel_welcome.add(userLabel);
+		userLabel.setFont(new Font("Arial", Font.BOLD, 14));
+		
+		JLabel creditLabel = new JLabel("App delivered by: Marucci, Morace");
+		creditLabel.setBounds(0, 572, 327, 44);
+		panel_Menu.add(creditLabel);
+		creditLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		creditLabel.setForeground(Color.WHITE);
+		creditLabel.setFont(new Font("Tahoma", Font.BOLD, 12));
+	}
+	
+	public void menuClicked(JPanel panel) {
+		paneHome.setVisible(false);
+		paneLibrary.setVisible(false);
+		paneSearch.setVisible(false);
+		paneInfo.setVisible(false);
+		
+		panel.setVisible(true);
+	}
+	
+	public class PanelButtonMouseAdapter extends MouseAdapter{
+		
+		JPanel panel;
+		public PanelButtonMouseAdapter(JPanel panel) {
+			this.panel=panel;
+		}
+		
+		@Override
+		public void mouseClicked(MouseEvent e) {
+			panel.setBackground(Color.DARK_GRAY);
+		}
+		@Override
+		public void mouseExited(MouseEvent e) {
+			panel.setBackground(Color.GRAY);	
+		}
+		@Override
+		public void mouseEntered(MouseEvent e) {
+			panel.setBackground(Color.DARK_GRAY);
+		}
+		@Override
+		public void mousePressed(MouseEvent e) {
+			panel.setBackground(Color.LIGHT_GRAY);
+		}
+		@Override
+		public void mouseReleased(MouseEvent e) {
+			panel.setBackground(Color.DARK_GRAY);
+		}
+	}
+}
---
+++ SoundLab/src/GUI/RegistrationGUI.java
@@ -0,0 +1,259 @@
+package GUI;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import java.awt.BorderLayout;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.SwingConstants;
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Image;
+import java.awt.Toolkit;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.text.SimpleDateFormat;
+
+import javax.swing.ImageIcon;
+import javax.swing.border.LineBorder;
+import javax.swing.JTextField;
+import javax.swing.JPasswordField;
+import com.toedter.calendar.JCalendar;
+import com.toedter.calendar.JDateChooser;
+import com.toedter.components.JSpinField;
+
+import Connessione.RegisterConnection;
+
+import com.toedter.calendar.JYearChooser;
+import com.toedter.calendar.JMonthChooser;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.sql.Date;
+
+public class RegistrationGUI {
+	int mouseX, mouseY;
+	private JTextField usernameField;
+	private JPasswordField passwordField;
+	private JTextField emailField;
+	private JTextField genderField;
+	
+	public RegistrationGUI() {
+		initialize();
+	}
+
+	/**
+	 * Initialize the contents of the frame.
+	 */
+	private void initialize() {
+		JFrame RegistrationFrame = new JFrame();
+		RegistrationFrame.setUndecorated(true);
+		RegistrationFrame.setResizable(false);
+		RegistrationFrame.setTitle("Registration");
+		RegistrationFrame.setVisible(true);
+		RegistrationFrame.setBounds(100, 100, 568, 624);
+		RegistrationFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		RegistrationFrame.getContentPane().setLayout(null);
+		
+		JPanel draggablePanel = new JPanel();
+		draggablePanel.setBounds(0, 0, 568, 20);
+		RegistrationFrame.getContentPane().add(draggablePanel);
+		draggablePanel.addMouseMotionListener(new MouseMotionAdapter() {
+			@Override
+			public void mouseDragged(MouseEvent e) {
+				RegistrationFrame.setLocation(RegistrationFrame.getX() + e.getX() - mouseX, RegistrationFrame.getY() + e.getY() - mouseY);
+			}
+		});
+		draggablePanel.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mousePressed(MouseEvent e) {
+				mouseX = e.getX();
+				mouseY = e.getY();
+			}
+		});
+		draggablePanel.setLayout(null);
+		draggablePanel.setBackground(Color.BLACK);
+		
+		JLabel exitButton = new JLabel("");
+		exitButton.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/closered.png")).getImage().getScaledInstance(15, 15, Image.SCALE_SMOOTH)));
+		exitButton.setHorizontalAlignment(SwingConstants.CENTER);
+		exitButton.setForeground(Color.WHITE);
+		exitButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				if(JOptionPane.showConfirmDialog(null, "Sei sicuro di voler uscire?", "Conferma", JOptionPane.YES_NO_OPTION) == 0) {
+					RegistrationFrame.dispose();
+				}
+			}
+		});
+		exitButton.setFont(new Font("Arial", Font.BOLD, 16));
+		exitButton.setBounds(541, -3, 27, 24);
+		draggablePanel.add(exitButton);
+		
+		JLabel minimizeButton = new JLabel("");
+		minimizeButton.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				RegistrationFrame.setExtendedState(JFrame.ICONIFIED);
+			}
+		});
+		minimizeButton.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/minimize.png")).getImage().getScaledInstance(18, 18, Image.SCALE_SMOOTH)));
+		minimizeButton.setHorizontalAlignment(SwingConstants.CENTER);
+		minimizeButton.setForeground(Color.WHITE);
+		minimizeButton.setFont(new Font("Arial", Font.BOLD, 16));
+		minimizeButton.setBounds(512, -3, 30, 27);
+		draggablePanel.add(minimizeButton);
+		
+		JPanel panelRegistration = new JPanel();
+		panelRegistration.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		panelRegistration.setBackground(Color.GRAY);
+		panelRegistration.setBounds(0, 20, 568, 604);
+		RegistrationFrame.getContentPane().add(panelRegistration);
+		panelRegistration.setLayout(null);
+		
+		JLabel FrontLogo = new JLabel("");
+		FrontLogo.setIcon(new ImageIcon(new ImageIcon(getClass().getResource("/Immagini/FrontLogo.png")).getImage().getScaledInstance(250, 250, Image.SCALE_SMOOTH)));
+		FrontLogo.setHorizontalAlignment(SwingConstants.CENTER);
+		FrontLogo.setBounds(164, 11, 230, 200);
+		panelRegistration.add(FrontLogo);
+		
+		JPanel UsernamePanel = new JPanel();
+		UsernamePanel.setLayout(null);
+		UsernamePanel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		UsernamePanel.setBackground(Color.WHITE);
+		UsernamePanel.setBounds(122, 222, 309, 53);
+		panelRegistration.add(UsernamePanel);
+		
+		usernameField = new JTextField();
+		usernameField.setFont(new Font("Arial", Font.PLAIN, 16));
+		usernameField.setColumns(10);
+		usernameField.setBounds(10, 11, 215, 35);
+		UsernamePanel.add(usernameField);
+		
+		JLabel usernameLabel = new JLabel("Username");
+		usernameLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		usernameLabel.setBounds(225, 2, 68, 53);
+		UsernamePanel.add(usernameLabel);
+		usernameLabel.setFont(new Font("Arial", Font.BOLD, 12));
+		
+		JPanel PasswordPanel = new JPanel();
+		PasswordPanel.setLayout(null);
+		PasswordPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		PasswordPanel.setBackground(Color.WHITE);
+		PasswordPanel.setBounds(122, 277, 309, 53);
+		panelRegistration.add(PasswordPanel);
+		
+		passwordField = new JPasswordField();
+		passwordField.setFont(new Font("Arial", Font.PLAIN, 16));
+		passwordField.setBounds(10, 11, 215, 35);
+		PasswordPanel.add(passwordField);
+		
+		JLabel passwordLabel = new JLabel("Password");
+		passwordLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		passwordLabel.setBounds(225, 2, 68, 53);
+		PasswordPanel.add(passwordLabel);
+		passwordLabel.setFont(new Font("Arial", Font.BOLD, 12));
+		
+		JPanel EmailPanel = new JPanel();
+		EmailPanel.setLayout(null);
+		EmailPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		EmailPanel.setBackground(Color.WHITE);
+		EmailPanel.setBounds(122, 332, 309, 53);
+		panelRegistration.add(EmailPanel);
+		
+		emailField = new JTextField();
+		emailField.setFont(new Font("Arial", Font.PLAIN, 16));
+		emailField.setColumns(10);
+		emailField.setBounds(10, 11, 215, 35);
+		EmailPanel.add(emailField);
+		
+		JLabel emailLabel = new JLabel("Email");
+		emailLabel.setBounds(224, 2, 46, 53);
+		EmailPanel.add(emailLabel);
+		emailLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		emailLabel.setFont(new Font("Arial", Font.BOLD, 12));
+		
+		JPanel GenderPanel = new JPanel();
+		GenderPanel.setLayout(null);
+		GenderPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		GenderPanel.setBackground(Color.WHITE);
+		GenderPanel.setBounds(122, 387, 309, 53);
+		panelRegistration.add(GenderPanel);
+		
+		genderField = new JTextField();
+		genderField.setFont(new Font("Arial", Font.PLAIN, 16));
+		genderField.setColumns(10);
+		genderField.setBounds(10, 11, 215, 35);
+		GenderPanel.add(genderField);
+		
+		JLabel genderLabel = new JLabel("Sesso");
+		genderLabel.setBounds(225, 2, 46, 53);
+		GenderPanel.add(genderLabel);
+		genderLabel.setHorizontalAlignment(SwingConstants.CENTER);
+		genderLabel.setFont(new Font("Arial", Font.BOLD, 12));
+		
+		JPanel DataPanel = new JPanel();
+		DataPanel.setLayout(null);
+		DataPanel.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		DataPanel.setBackground(Color.WHITE);
+		DataPanel.setBounds(122, 442, 309, 53);
+		panelRegistration.add(DataPanel);
+		
+		JDateChooser dateChooser = new JDateChooser();
+		dateChooser.setBounds(10, 11, 215, 35);
+		DataPanel.add(dateChooser);
+        dateChooser.setDateFormatString("y/MM/d");
+        
+        JLabel nascitaLabel = new JLabel("Data nascita");
+        nascitaLabel.setBounds(215, 0, 102, 53);
+        DataPanel.add(nascitaLabel);
+        nascitaLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        nascitaLabel.setFont(new Font("Arial", Font.BOLD, 12));
+		
+		JButton Register_Button = new JButton("REGISTER");
+		Register_Button.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				String eccoUs = usernameField.getText();
+				String eccoPsd = passwordField.getText();
+				String eccoEmail = emailField.getText();
+				String eccoGender = genderField.getText();
+				Date eccoData = new Date(dateChooser.getDate().getTime());
+				boolean ok;
+					
+				RegisterConnection registrazione = new RegisterConnection();
+				ok = registrazione.ritornaRegistrazione(eccoUs, eccoPsd, eccoEmail, eccoGender, eccoData);
+				
+				if(ok == true) {
+					JOptionPane.showMessageDialog(null, "Registrazione effettuata con successo!.");
+					RegistrationFrame.dispose();
+				}
+			}
+		});
+		
+		Register_Button.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseEntered(MouseEvent e) {
+				Register_Button.setBackground(Color.DARK_GRAY);
+			}
+			@Override
+			public void mouseExited(MouseEvent e) {
+				Register_Button.setBackground(Color.WHITE);	
+			}
+		});
+		Register_Button.setForeground(Color.BLACK);
+		Register_Button.setFont(new Font("Tahoma", Font.BOLD, 16));
+		Register_Button.setBorder(new LineBorder(new Color(0, 0, 0), 2, true));
+		Register_Button.setBackground(Color.WHITE);
+		Register_Button.setBounds(169, 510, 214, 62);
+		panelRegistration.add(Register_Button);
+	}
+}
